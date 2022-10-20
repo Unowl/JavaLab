71cd423c4418c760ae85e1487f20233b04018e50
Unowl <80276385+Unowl@users.noreply.github.com>
2022-10-20 22:12:01 +0300

    Update Task13.java

--- Task13.java
+++ Task13.java
@@ -1,13 +1,30 @@
-import java.io.*;
-public class Main
+public class Task13
 {
-   public static void main(String[] args) throws IOException
-   {
-       String greetings = "Привет!";
-        try(FileOutputStream output = new FileOutputStream("C:\\Users\\Public\\Documents"))
-        {
-            output.write(greetings.getBytes());
-        }   
-   }
+    public static class C implements AutoCloseable { 
+        boolean a;
+ 
+        @Override 
+        public void close() throws Exception { 
+            System.out.println("close"); 
+        } 
+        
+        public void doing() throws Exception{ 
+            if(a) System.out.println("TRUE");
+		    else throw new Exception("NOT TRUE");
+        } 
+    } 
+ 
+    public static void main(String[] arguments) throws Exception { 
+  
+	   try(C test = new C())
+	   {
+		   test.doing();
+	   }
+	   catch (Exception e) 
+	   {
+		   System.out.println(e.getMessage());
+	   }
+         
+    } 
 }
 
